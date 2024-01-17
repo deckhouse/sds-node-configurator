908d5920ad001da67fd60c6022deae8d1feae74c
viktor.kramarenko <viktor.kramarenko@flant.com>
2024-01-17 19:33:12 +0300

    first-draft Signed-off-by: viktor.kramarenko <viktor.kramarenko@flant.com>
    Signed-off-by: Viktor Kramarenko <viktor.kramarenko@flant.com>

--- images/agent/pkg/controller/lvm_logical_volume_watcher.go
+++ images/agent/pkg/controller/lvm_logical_volume_watcher.go
@@ -2,22 +2,26 @@ package controller
 
 import (
 	"context"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/util/workqueue"
 	"sds-node-configurator/api/v1alpha1"
 	"sds-node-configurator/config"
 	"sds-node-configurator/pkg/logger"
 	"sds-node-configurator/pkg/monitoring"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/event"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 	"sigs.k8s.io/controller-runtime/pkg/source"
+	"time"
 )
 
 const (
 	LVMLogicalVolumeWatcherCtrlName = "lvm-logical-volume-watcher-controller"
 )
 
-func RunLVMLogicalVolumeController(
+func RunLVMLogicalVolumeWatcherController(
 	mgr manager.Manager,
 	cfg config.Options,
 	log logger.Logger,
@@ -28,24 +32,59 @@ func RunLVMLogicalVolumeController(
 
 	c, err := controller.New(LVMLogicalVolumeWatcherCtrlName, mgr, controller.Options{
 		Reconciler: reconcile.Func(func(ctx context.Context, request reconcile.Request) (reconcile.Result, error) {
-			return reconcile.Result{}, nil
+			log.Info("[RunLVMLogicalVolumeWatcherController] HELLO FROM RECONCILER")
+			return reconcile.Result{
+				RequeueAfter: 5 * time.Second,
+			}, nil
 		}),
 	})
 	if err != nil {
-		log.Error(err, "[RunLVMLogicalVolumeController] unable to create controller")
+		log.Error(err, "[RunLVMLogicalVolumeWatcherController] unable to create controller")
 		return nil, err
 	}
 
+	// lvremove
+	createFunc := func(ctx context.Context, e event.CreateEvent, q workqueue.RateLimitingInterface) {
+		log.Info("[RunLVMLogicalVolumeWatcherController] Hello from CreateFunc")
+		request := reconcile.Request{NamespacedName: types.NamespacedName{
+			Namespace: e.Object.GetNamespace(),
+			Name:      e.Object.GetName(),
+		}}
+		log.Info("NAME:" + request.Name)
+		log.Info("NAMESPACE:" + request.Namespace)
+		// Если создается ресурс llv, я
+		// - должен получить информацию о том, где я должен создавать PV
+		// - я проверяю возможность создания PV в указанном месте
+		// - пытаюсь создать PV нужного размера
+		// - если создаю, то обновляю статус ресурса успехом, проставляю актуальные данные
+		// - если нет, кидаю ошибку и сообщение
+	}
+
 	err = c.Watch(source.Kind(cache, &v1alpha1.LvmLogicalVolume{}), handler.Funcs{
-		CreateFunc:  nil,
-		UpdateFunc:  nil,
-		DeleteFunc:  nil,
-		GenericFunc: nil,
+		CreateFunc: createFunc,
 	})
 	if err != nil {
-		log.Error(err, "[RunLVMLogicalVolumeController] the controller is unable to watch")
+		log.Error(err, "[RunLVMLogicalVolumeWatcherController] the controller is unable to watch")
 		return nil, err
 	}
 
 	return c, err
+}
+
+func updateFunc() {
+	// Если обновляется спека llv, я
+	// - проверяю текущий статус ресурса
+	// - если статус соответствует спеке, ничего не делаю
+	// - если статус не соответствует спеке, тогда
+	// - я проверяю возможность создания PV в указанном месте
+	// - пытаюсь создать PV нужного размера
+	// - если создаю, то обновляю статус ресурса успехом, проставляю актуальные данные
+	// - если нет, кидаю ошибку и сообщение
+}
+
+func deleteFunc() {
+	// Если удаляется ресурс llv, я
+	// - получаю данные для удаления из спеки ресурса
+	// - выполняю удаления
+	// - если удачно, то
 }
