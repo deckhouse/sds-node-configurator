6ef02973b67c8b3392a7a06f804c5b3788c42308
Nikolay Demchuk <nikolay.demchuk@flant.com>
2024-07-25 18:37:13 +1000

    test Signed-off-by: Nikolay Demchuk <nikolay.demchuk@flant.com>
    Signed-off-by: Aleksandr Zimin <alexandr.zimin@flant.com>

--- images/agent/src/pkg/controller/block_device.go
+++ images/agent/src/pkg/controller/block_device.go
@@ -553,7 +553,7 @@ func GetBlockDeviceLabels(blockDevice v1alpha1.BlockDevice) map[string]string {
 		blockDevice.Labels = make(map[string]string)
 	}
 
-	blockDevice.Labels["kubernetes.io/metadata.name"] = blockDevice.ObjectMeta.Name
+	// blockDevice.Labels["kubernetes.io/metadata.name"] = blockDevice.ObjectMeta.Name
 	blockDevice.Labels["kubernetes.io/hostname"] = blockDevice.Status.NodeName
 	blockDevice.Labels[BlockDeviceLabelPrefix+"/type"] = blockDevice.Status.Type
 	blockDevice.Labels[BlockDeviceLabelPrefix+"/fstype"] = blockDevice.Status.FsType
