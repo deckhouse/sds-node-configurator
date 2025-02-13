91a53f608142ba4a8482fa01de43988595aaa26d
Anton Sergunov <anton.sergunov@flant.com>
2025-02-13 18:06:53 +0600

    add log info for cleaning
    Signed-off-by: Anton Sergunov <anton.sergunov@flant.com>

--- images/agent/src/internal/utils/volume_cleanup_ee.go
+++ images/agent/src/internal/utils/volume_cleanup_ee.go
@@ -20,6 +20,7 @@ import (
 )
 
 func VolumeCleanup(ctx context.Context, log logger.Logger, vgName, lvName, volumeCleanupMethod string) error {
+	log.Info("Cleaning up volume", "vgname", vgName, "lvname", lvName, "method", volumeCleanupMethod)
 	if !commonfeature.VolumeCleanupEnabled() {
 		return fmt.Errorf("Volume cleanup is not supported in your edition.")
 	}
