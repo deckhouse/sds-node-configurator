c45388aa33bb74b302e22e68aeedc9532097a533
Anton Sergunov <anton.sergunov@flant.com>
2025-02-14 11:02:27 +0600

    use fstat call
    Signed-off-by: Anton Sergunov <anton.sergunov@flant.com>

--- images/agent/src/internal/utils/volume_cleanup_ee.go
+++ images/agent/src/internal/utils/volume_cleanup_ee.go
@@ -64,6 +64,10 @@ func volumeSize(stat syscall.Stat_t) (int64, error) {
 		return stat.Size, nil
 	}
 
+	if stat.Mode&S_IFMT != S_IFBLK {
+		return 0, fmt.Errorf("not a block device, ifmt: %x", stat.Mode&S_IFMT)
+	}
+
 	if stat.Blksize <= 0 {
 		return 0, fmt.Errorf("block size %d is invalid, stat: %v", stat.Blksize, stat)
 	}
@@ -75,10 +79,6 @@ func volumeSize(stat syscall.Stat_t) (int64, error) {
 }
 
 func volumeCleanupCopy(ctx context.Context, log logger.Logger, closingErrors *[]error, outputPath, inputPath string, passes int) error {
-	outputStat, err := volumeStat(outputPath)
-	if err != nil {
-		return fmt.Errorf("can't get stat of %s: %w", outputPath, err)
-	}
 
 	close := func(file *os.File) {
 		log := log.GetLogger().WithValues("name", file.Name())
@@ -99,6 +99,10 @@ func volumeCleanupCopy(ctx context.Context, log logger.Logger, closingErrors *[]
 	output, err := os.OpenFile(outputPath, syscall.O_DIRECT, os.ModeDevice)
 	if err != nil {
 		return fmt.Errorf("opening device %s to wipe: %w", outputPath, err)
+	}
+	outputStat, err := volumeStat(output)
+	if err != nil {
+		return fmt.Errorf("can't get stat of %s: %w", outputPath, err)
 	}
 	defer close(output)
 
@@ -129,22 +133,52 @@ const (
 	BLKDISCARD       = 0x1277
 	BLKDISCARDZEROES = 0x127c
 	BLKSECDISCARD    = 0x127d
+
+	S_IFMT   = 0x0170000 /* type of file mask */
+	S_IFIFO  = 0x0010000 /* named pipe (fifo) */
+	S_IFCHR  = 0x0020000 /* character special */
+	S_IFDIR  = 0x0040000 /* directory */
+	S_IFBLK  = 0x0060000 /* block special */
+	S_IFREG  = 0x0100000 /* regular */
+	S_IFLNK  = 0x0120000 /* symbolic link */
+	S_IFSOCK = 0x0140000 /* socket */
+	S_ISUID  = 0x0004000 /* set-user-ID on execution */
+	S_ISGID  = 0x0002000 /* set-group-ID on execution */
+	S_ISVTX  = 0x0001000 /* save swapped text even after use */
+	S_IRWXU  = 0x0000700 /* RWX mask for owner */
+	S_IRUSR  = 0x0000400 /* R for owner */
+	S_IWUSR  = 0x0000200 /* W for owner */
+	S_IXUSR  = 0x0000100 /* X for owner */
+	S_IRWXG  = 0x0000070 /* RWX mask for group */
+	S_IRGRP  = 0x0000040 /* R for group */
+	S_IWGRP  = 0x0000020 /* W for group */
+	S_IXGRP  = 0x0000010 /* X for group */
+	S_IRWXO  = 0x0000007 /* RWX mask for other */
+	S_IROTH  = 0x0000004 /* R for other */
+	S_IWOTH  = 0x0000002 /* W for other */
+	S_IXOTH  = 0x0000001 /* X for other */
 )
 
 type Range struct {
 	start, count uint64
 }
 
-func volumeStat(devicePath string) (syscall.Stat_t, error) {
+func volumeStat(device *os.File) (syscall.Stat_t, error) {
 	var stat syscall.Stat_t
-	if err := syscall.Stat(devicePath, &stat); err != nil {
-		return stat, fmt.Errorf("stat call failed for device %s: %w", devicePath, err)
+	if err := syscall.Fstat(int(device.Fd()), &stat); err != nil {
+		return stat, fmt.Errorf("fstat call failed: %w", err)
 	}
 	return stat, nil
 }
 
 func volumeCleanupDiscard(ctx context.Context, log logger.Logger, closingErrors *[]error, devicePath string) error {
-	stat, err := volumeStat(devicePath)
+
+	device, err := os.OpenFile(devicePath, syscall.O_DIRECT, os.ModeDevice)
+	if err != nil {
+		return fmt.Errorf("opening device %s to wipe: %w", devicePath, err)
+	}
+
+	stat, err := volumeStat(device)
 	if err != nil {
 		return fmt.Errorf("can't get stat of %s: %w", devicePath, err)
 	}
@@ -152,11 +186,6 @@ func volumeCleanupDiscard(ctx context.Context, log logger.Logger, closingErrors 
 	deviceSize, err := volumeSize(stat)
 	if err != nil {
 		return fmt.Errorf("can't find the size of device %s: %w", devicePath, err)
-	}
-
-	device, err := os.OpenFile(devicePath, syscall.O_DIRECT, os.ModeDevice)
-	if err != nil {
-		return fmt.Errorf("opening device %s to wipe: %w", devicePath, err)
 	}
 	defer func() {
 		log.Info("Closing file", device)
