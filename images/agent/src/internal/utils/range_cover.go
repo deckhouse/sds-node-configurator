92470b28b5f649ffa5a7932611e75d7441767bf9
Anton Sergunov <anton.sergunov@flant.com>
2025-02-25 18:44:20 +0600

    Range Covers
    Signed-off-by: Anton Sergunov <anton.sergunov@flant.com>

---
+++ images/agent/src/internal/utils/range_cover.go
@@ -0,0 +1,76 @@
+/*
+Copyright 2023 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package utils
+
+import (
+	"fmt"
+	"sort"
+)
+
+type Range struct {
+	Start, Count int64
+}
+
+type RangeCover []Range
+
+func (rc RangeCover) Merged() (RangeCover, error) {
+	rcLen := len(rc)
+	if rcLen <= 1 {
+		return rc, nil
+	}
+
+	sort.Slice(rc, func(i, j int) bool {
+		return rc[i].Start < rc[j].Start
+	})
+
+	for i, d := range rc[0 : rcLen-1] {
+		if d.Start+d.Count > rc[i+1].Start {
+			return nil, fmt.Errorf("self overlapped range")
+		}
+	}
+
+	last := Range{Start: 0, Count: 0}
+	reduced := rc[:0]
+	for _, d := range rc {
+		if last.Count == 0 {
+			last = d
+		} else if d.Start == last.Start+last.Count {
+			last.Count += d.Count
+		} else {
+			reduced = append(reduced, last)
+			last = d
+		}
+	}
+
+	if last.Count > 0 {
+		reduced = append(reduced, last)
+	}
+
+	return reduced, nil
+}
+
+func (value Range) Multiplied(multiplier int64) Range {
+	return Range{Start: value.Start * multiplier, Count: value.Count * multiplier}
+}
+
+func (cover RangeCover) Multiplied(multiplier int64) RangeCover {
+	multiplied := make([]Range, len(cover))
+	for i, value := range cover {
+		multiplied[i] = value.Multiplied(multiplier)
+	}
+	return multiplied
+}
---
+++ images/agent/src/internal/utils/range_cover_test.go
@@ -0,0 +1,98 @@
+/*
+Copyright 2023 Flant JSC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package utils
+
+import (
+	"slices"
+	"testing"
+)
+
+func TestMergeEmpty(t *testing.T) {
+	result, err := RangeCover{}.Merged()
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	if len(result) != 0 {
+		t.Errorf("expected empty range, got %v", result)
+	}
+}
+
+func TestMergeOverlapped(t *testing.T) {
+	_, err := RangeCover{
+		Range{Start: 0, Count: 2},
+		Range{Start: 1, Count: 1},
+	}.Merged()
+	if err == nil {
+		t.Errorf("unexpected success: %v", err)
+	}
+
+	if err.Error() != "self overlapped range" {
+		t.Errorf("expected error %v", err)
+	}
+}
+
+func TestMergeNoMerge(t *testing.T) {
+	original := RangeCover{
+		Range{Start: 0, Count: 2},
+		Range{Start: 7, Count: 1},
+	}
+	result, err := original.Merged()
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	if !slices.Equal(original, result) {
+		t.Errorf("expected %v, got %v", original, result)
+	}
+}
+
+func TestMergeMerge(t *testing.T) {
+	original := RangeCover{
+		Range{Start: 0, Count: 2},
+		Range{Start: 2, Count: 1},
+	}
+	expected := RangeCover{
+		Range{Start: 0, Count: 3},
+	}
+	result, err := original.Merged()
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+
+	if !slices.Equal(expected, result) {
+		t.Errorf("expected %v, got %v", expected, result)
+	}
+}
+
+func TestMultiply(t *testing.T) {
+	multiplier := int64(4)
+	original := RangeCover{
+		Range{Start: 0, Count: 2},
+		Range{Start: 5, Count: 1},
+	}
+	expected := RangeCover{
+		Range{Start: 0, Count: 8},
+		Range{Start: 20, Count: 4},
+	}
+
+	result := original.Multiplied(multiplier)
+
+	if !slices.Equal(result, expected) {
+		t.Errorf("expected %v, got %v", expected, result)
+	}
+}
