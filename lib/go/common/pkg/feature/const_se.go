77f6b77255a9b28a4535315e6216c8dbb4b0151e
Anton Sergunov <anton.sergunov@flant.com>
2025-04-04 19:33:40 +0600

    pre-commit hooks for go
    Signed-off-by: Anton Sergunov <anton.sergunov@flant.com>

--- lib/go/common/pkg/feature/const_se.go
+++ lib/go/common/pkg/feature/const_se.go
@@ -2,7 +2,7 @@
 
 /*
 Copyright 2025 Flant JSC
-Licensed under the Deckhouse Platform Enterprise Edition (EE) license.
+  Licensed under the Deckhouse Platform Enterprise Edition (EE) license.
 See https://github.com/deckhouse/deckhouse/blob/main/ee/LICENSE
 */
 
