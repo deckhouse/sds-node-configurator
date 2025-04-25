cfc690a24588a55ed7670b058ad1d6eafda905be
Slava V <vyacheslav.voytenok@flant.com>
2025-04-25 18:28:55 +0700

    changed namespace const value
    Signed-off-by: Slava V <vyacheslav.voytenok@flant.com>

--- images/sds-common-scheduler-extender/cmd/main.go
+++ images/sds-common-scheduler-extender/cmd/main.go
@@ -166,7 +166,7 @@ func subMain(ctx context.Context) error {
 	}
 
 	сache := cache.NewCache(log)
-	cacheMrg := cache.NewCacheManager(сache, &sync.Mutex{}, log)
+	cacheMrg := cache.NewCacheManager(сache, &sync.Mutex{}, mgr, log)
 	log.Info("[subMain] scheduler cache manager initialized")
 
 	go cacheMrg.RunCleaner(ctx, time.Duration(config.CachePVCCheckInterval))
