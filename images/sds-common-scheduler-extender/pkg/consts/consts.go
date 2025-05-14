f0300df1b475b9bb59c2c9e9099e6e86433a4ec3
Slava V <vyacheslav.voytenok@flant.com>
2025-05-14 18:37:04 +0700

    shouldProcess upd
    Signed-off-by: Slava V <vyacheslav.voytenok@flant.com>

--- images/sds-common-scheduler-extender/pkg/consts/consts.go
+++ images/sds-common-scheduler-extender/pkg/consts/consts.go
@@ -2,6 +2,7 @@ package consts
 
 const (
 	SdsReplicatedVolumeProvisioner = "replicated.csi.storage.deckhouse.io"
+	SdsLocalVolumeProvisioner      = "local.csi.storage.deckhouse.io"
 
 	LvmTypeParamKey         = "local.csi.storage.deckhouse.io/lvm-type"
 	LVMVolumeGroupsParamKey = "local.csi.storage.deckhouse.io/lvm-volume-groups"
