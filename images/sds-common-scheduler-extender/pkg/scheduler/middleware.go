b60a740f207d581ae368a89d0550a84270eef267
Slava V <vyacheslav.voytenok@flant.com>
2025-05-14 20:58:33 +0700

    fix middleware again
    Signed-off-by: Slava V <vyacheslav.voytenok@flant.com>

--- images/sds-common-scheduler-extender/pkg/scheduler/middleware.go
+++ images/sds-common-scheduler-extender/pkg/scheduler/middleware.go
@@ -30,79 +30,77 @@ func NewMiddleware(handler http.Handler, log *logger.Logger) *Middleware {
 }
 
 func (m *Middleware) WithLog() *Middleware {
-	return &Middleware{
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			m.Handler.ServeHTTP(w, r)
+	m.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		m.Handler.ServeHTTP(w, r)
 
-			startTime := time.Now()
-			// status := m.handler.Status
+		startTime := time.Now()
+		// status := m.handler.Status
 
-			fields := []interface{}{
-				"type", "access",
-				"response_time", time.Since(startTime).Seconds(),
-				"protocol", r.Proto,
-				"http_status_code", status,
-				"http_method", r.Method,
-				"url", r.RequestURI,
-				"http_host", r.Host,
-				"request_size", r.ContentLength,
-				// "response_size", wr.size,
-			}
-			ip, _, err := net.SplitHostPort(r.RemoteAddr)
-			if err == nil {
-				fields = append(fields, "remote_ipaddr", ip)
-			}
-			ua := r.Header.Get("User-Agent")
-			if len(ua) > 0 {
-				fields = append(fields, "http_user_agent", ua)
-			}
-			m.Log.Info("access", fields...)
-		}),
-	}
+		fields := []interface{}{
+			"type", "access",
+			"response_time", time.Since(startTime).Seconds(),
+			"protocol", r.Proto,
+			"http_status_code", status,
+			"http_method", r.Method,
+			"url", r.RequestURI,
+			"http_host", r.Host,
+			"request_size", r.ContentLength,
+			// "response_size", wr.size,
+		}
+		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		if err == nil {
+			fields = append(fields, "remote_ipaddr", ip)
+		}
+		ua := r.Header.Get("User-Agent")
+		if len(ua) > 0 {
+			fields = append(fields, "http_user_agent", ua)
+		}
+		m.Log.Info("access", fields...)
+	})
+	return m
 }
 
 func (m *Middleware) WithPodCheck(ctx context.Context, cl client.Client) *Middleware {
-	return &Middleware{
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			var inputData ExtenderArgs
+	m.Handler = http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var inputData ExtenderArgs
 
-			reader := http.MaxBytesReader(w, r.Body, 10<<20)
-			if err := json.NewDecoder(reader).Decode(&inputData); err != nil {
+		reader := http.MaxBytesReader(w, r.Body, 10<<20)
+		if err := json.NewDecoder(reader).Decode(&inputData); err != nil {
+			m.Log.Error(err, "[ShouldProcessPodMiddleware] unable to decode filter request")
+			httpError(w, "unable to decode request", http.StatusBadRequest)
+			return
+		}
+		pod := inputData.Pod
+		if pod == nil {
+			m.Log.Error(errors.New("[ShouldProcessPodMiddleware] no pod in request"), "")
+			httpError(w, "[ShouldProcessPodMiddleware] no pod in request", http.StatusInternalServerError)
+		}
+
+		pvcs := &corev1.PersistentVolumeClaimList{}
+		if err := cl.List(ctx, pvcs); err != nil {
+			m.Log.Error(err, "[shouldProcessPodMiddleware] error listing PVCs")
+			http.Error(w, "error listing PVCs", http.StatusInternalServerError)
+		}
+
+		pvcMap := make(map[string]*corev1.PersistentVolumeClaim, len(pvcs.Items))
+		for _, pvc := range pvcs.Items {
+			pvcMap[pvc.Name] = &pvc
+		}
+
+		volumes, err := shouldProcessPod(ctx, cl, pvcMap, m.Log, pod)
+		if err != nil {
+			m.Log.Error(err, fmt.Sprintf("[shouldProcessPodMiddleware] error processing pod %s/%s: %v", pod.Namespace, pod.Name))
+			result := &ExtenderFilterResult{NodeNames: inputData.NodeNames}
+			if err := json.NewEncoder(w).Encode(result); err != nil {
 				m.Log.Error(err, "[ShouldProcessPodMiddleware] unable to decode filter request")
 				httpError(w, "unable to decode request", http.StatusBadRequest)
 				return
 			}
-			pod := inputData.Pod
-			if pod == nil {
-				m.Log.Error(errors.New("[ShouldProcessPodMiddleware] no pod in request"), "")
-				httpError(w, "[ShouldProcessPodMiddleware] no pod in request", http.StatusInternalServerError)
-			}
+			return
+		}
 
-			pvcs := &corev1.PersistentVolumeClaimList{}
-			if err := cl.List(ctx, pvcs); err != nil {
-				m.Log.Error(err, "[shouldProcessPodMiddleware] error listing PVCs")
-				http.Error(w, "error listing PVCs", http.StatusInternalServerError)
-			}
-
-			pvcMap := make(map[string]*corev1.PersistentVolumeClaim, len(pvcs.Items))
-			for _, pvc := range pvcs.Items {
-				pvcMap[pvc.Name] = &pvc
-			}
-
-			volumes, err := shouldProcessPod(ctx, cl, pvcMap, m.Log, pod)
-			if err != nil {
-				m.Log.Error(err, fmt.Sprintf("[shouldProcessPodMiddleware] error processing pod %s/%s: %v", pod.Namespace, pod.Name))
-				result := &ExtenderFilterResult{NodeNames: inputData.NodeNames}
-				if err := json.NewEncoder(w).Encode(result); err != nil {
-					m.Log.Error(err, "[ShouldProcessPodMiddleware] unable to decode filter request")
-					httpError(w, "unable to decode request", http.StatusBadRequest)
-					return
-				}
-				return
-			}
-
-			m.Log.Trace(fmt.Sprintf("[shouldProcessPodMiddleware] pod %s/%s is eligible, matched volumes: %+v", pod.Namespace, pod.Name, volumes))
-			m.Handler.ServeHTTP(w, r)
-		}),
-	}
+		m.Log.Trace(fmt.Sprintf("[shouldProcessPodMiddleware] pod %s/%s is eligible, matched volumes: %+v", pod.Namespace, pod.Name, volumes))
+		m.Handler.ServeHTTP(w, r)
+	})
+	return m
 }
