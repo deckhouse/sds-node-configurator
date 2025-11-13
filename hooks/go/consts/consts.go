03125605d22c1e53f657c9b9ef4318842f7bcefa
Pavel Karpov <pavel.karpov@flant.com>
2025-11-13 09:35:34 +0300

    add cert to sds-common-scheduler-extender
    Signed-off-by: Pavel Karpov <pavel.karpov@flant.com>

--- hooks/go/consts/consts.go
+++ hooks/go/consts/consts.go
@@ -17,8 +17,10 @@ limitations under the License.
 package consts
 
 const (
-	ModuleName       string = "sdsNodeConfigurator"
-	ModuleNamespace  string = "d8-sds-node-configurator"
-	ModulePluralName string = "sds-node-configurator"
-	WebhookCertCn    string = "webhooks"
+	ModuleName                        string = "sdsNodeConfigurator"
+	ModuleNamespace                   string = "d8-sds-node-configurator"
+	ModulePluralName                  string = "sds-node-configurator"
+	WebhookCertCn                     string = "webhooks"
+	SdsCommonSchedulerExtenderCertCn  string = "sds-common-scheduler-extender"
+	CommonSchedulerExtenderSecretName string = "common-scheduler-extender-https-certs"
 )
