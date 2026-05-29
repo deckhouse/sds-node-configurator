a3b702a3dc9463a94548ef85e0ac144d90aac417
Daniil Studenikin <daniil.studenikin@flant.ru>
2026-05-29 11:47:59 +0300

    remove test Skip and make log
    Signed-off-by: Daniil Studenikin <daniil.studenikin@flant.ru>

--- images/agent/internal/scanner/scanner.go
+++ images/agent/internal/scanner/scanner.go
@@ -1,17 +1,17 @@
 /*
-Copyright 2025 Flant JSC
+	Copyright 2026 Flant JSC
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+	Licensed under the Apache License, Version 2.0 (the "License");
+	you may not use this file except in compliance with the License.
+	You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+		http://www.apache.org/licenses/LICENSE-2.0
 
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
+	Unless required by applicable law or agreed to in writing, software
+	distributed under the License is distributed on an "AS IS" BASIS,
+	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+	See the License for the specific language governing permissions and
+	limitations under the License.
 */
 
 package scanner
@@ -124,7 +124,7 @@ func (s *scanner) Run(
 			if err := s.deviceMap.HandleEvent(device.Action, device.Env); err != nil {
 				log.Error(err, fmt.Sprintf("[RunScanner] handle event error: %s", device.String()))
 			}
-			log.Info(fmt.Sprintf("Success handle netlink event on action %s", device.Action.String()))
+			log.Info(fmt.Sprintf("[HandleEvent] udev event action=%s devname=%s", device.Action.String(), device.Env["DEVNAME"]))
 
 			t.Do(func() {
 				log.Info("[RunScanner] start to fill the cache")
